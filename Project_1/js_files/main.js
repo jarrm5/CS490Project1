b4eb4c74df640d1593262ae570623d4044d4766a
jarrm5 <rinellaj2448@gmail.com>
2016-10-09 23:09:10 -0500

    added search feature so that only suggestions appear; page still unresponsive to search requests.

--- Project_1/js_files/main.js
+++ Project_1/js_files/main.js
@@ -8,6 +8,9 @@ function init(){
     //Loads the gridview
     dynamic_load(data,"#gridview-template","#gridview");
     
+    $("#search_button").on('click',search);
+    $("#field").on('keyup',search);
+    
     //Loads the listview
     
 }
@@ -20,6 +23,39 @@ function dynamic_load(data, template_id, destination){
     var html = html_maker.getHTML(data);
     //Load the template and data to a destination tag
     $(destination).html(html);
+}
+function search(){
+    var data = movies["movies"];
+    var keys = ["title","year","starring"];
+    var html = "";
+    var value = $("#field").val();
+    var show = false;
+    
+    for(var i=0;i<data.length;++i){
+        for(var j=0;j<keys.length;++j){
+            var start = data[i][keys[j]].toString().toLowerCase().search(value.toLowerCase().trim());
+            if (start != -1) { //if there is a search match
+            html += "<div class='sub_suggestions' data-item='" + data[i][keys[j]] + "' >";
+            html += data[i][keys[j]].substring(0,start)+"<b>"+data[i][keys[j]].substring(start,start+value.length)+"</b>"+data[i][keys[j]].substring(start+value.length,data[i][keys[j]].length);
+            html += "</div>";
+            show=true; //show suggestions
+            }
+        }
+    }
+    if(show){
+        $("#suggestions_box").html(html);
+        //get the children of suggestions_box with .sub_suggestions class
+        $("#suggestions_box").children(".sub_suggestions").on('click',function(){
+            var item=$(this).attr('data-item'); //get the data
+            $("#field").val(item); //show it in the field
+            $("#suggestions_box").hide(); //hide the suggestion box
+        });
+        
+        $("#suggestions_box").show();
+    }
+    else{
+       $("#suggestions_box").hide();
+    }
 }
 
 
